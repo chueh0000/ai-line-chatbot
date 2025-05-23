4ede58813ccdd4bdfa3082716ee87286bf4b8cdd
chueh <b10902076@csie.ntu.edu.tw>
2025-05-23 11:10:55 +0800

    switch to GCP on Vercel

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -7,8 +7,8 @@ const client = new line.messagingApi.MessagingApiClient({
 
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY,
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 })
